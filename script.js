de07629501b5a580ddc6b94658c3ab30d6fff4e3
Liam <liam.tangalin13@gmail.com>
2025-02-11 23:20:05 -0500

    s

--- script.js
+++ script.js
@@ -16,17 +16,17 @@ let messageIndex = 0;
 let love_count = 0
 
 function handleNoClick() {
-    const noButton = document.querySelector('.no-button');
-    const yesButton = document.querySelector('.yes-button');
-    noButton.textContent = messages[messageIndex];
-    messageIndex = (messageIndex + 1) % messages.length;
-    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
     love_count += 1
     if (love_count == 10) {
         window.location.href = "no_page.html";
-    }
-
+    } else {
+        const noButton = document.querySelector('.no-button');
+        const yesButton = document.querySelector('.yes-button');
+        noButton.textContent = messages[messageIndex];
+        messageIndex = (messageIndex + 1) % messages.length;
+        const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+        yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    } 
 }
 
 function handleYesClick() {
